7ce5f3bea4e21a9091999786274870806a533574
Kwasi Ankomah <kwasi.ankomah@sambanovasystems.com>
2025-02-17 15:18:32 +0000

    merge current front end into develop

---
+++ frontend/sales-agent-crew/src/components/ChatMain/ResponseTypes/UnknownTypeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <p>Unknown agent type: {{ parsed.agent_type }}</p>
+      <p> {{ parsed.data }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    parsed: {
+      type: Object,
+      required: true
+    }
+  });
+  </script>
+  

089ce1f11201263e671aa94a08b4a9d4ee4ea7fb
arund <arun.dua@sambanovasystems.com>
2025-02-17 16:46:36 +0530

    icons added

--- frontend/sales-agent-crew/src/components/Common/UIComponents/UserAvtar.vue
+++ frontend/sales-agent-crew/src/components/Common/UIComponents/UserAvtar.vue
@@ -1,7 +1,16 @@
+<template>
+    <span :class="avatarClass">
+      <span class="text-sm font-medium text-white leading-none">
+          {{ getInitials(user.firstName, user.lastName) }}
+      </span>
+    </span>
+  </template>
+  
 <script setup>
 import { computed } from 'vue'
 
 import { useUser } from '@clerk/vue'
+const { user, isLoaded, isSignedIn } = useUser()
 
 const props = defineProps({
   initials: {
@@ -15,17 +24,19 @@ const props = defineProps({
 })
 
 const avatarClass = computed(() => `shrink-0 inline-flex items-center justify-center size-[38px] rounded-md ${props.bgColor}`)
-</script>
 
-<template>
-  <span :class="avatarClass">
-    <span class="text-sm font-medium text-white leading-none">
-      AD
-    </span>
-  </span>
-</template>
-<script setup>
 
+
+function getInitials(firstName, lastName) {
+  let initials = '';
+  if (firstName && typeof firstName === 'string' && firstName.trim().length > 0) {
+    initials += firstName.trim().charAt(0).toUpperCase();
+  }
+  if (lastName && typeof lastName === 'string' && lastName.trim().length > 0) {
+    initials += lastName.trim().charAt(0).toUpperCase();
+  }
+  return initials;
+}
 </script>
 <style scoped>
 /* Scoped CSS if any additional styling is needed */

655f75712a44cb6288bbd6cb05d1175c3fecd077
arund <arun.dua@sambanovasystems.com>
2025-03-06 21:53:47 +0530

    logo added to the login page

--- frontend/sales-agent-crew/src/views/LoginPage.vue
+++ frontend/sales-agent-crew/src/views/LoginPage.vue
@@ -2,11 +2,11 @@
   <div class="min-h-screen bg-gradient-to-br from-primary-50 to-blue-50 flex items-center justify-center p-4">
     <div class="max-w-md w-full flex flex-col justify-center items-center ">
       <div class="text-center mb-8">
-        <!-- <img 
+        <img 
           src="https://sambanova.ai/hubfs/sambanova-logo-black.png" 
           alt="SambaNova Logo" 
           class="h-12 mx-auto mb-6"
-        /> -->
+        />
         <h1 class="text-4xl font-bold text-primary-brandTextPrimary mb-2">Agents</h1>
         <p class="text-lg text-primary-brandTextSecondary">Agent Powered Intelligence</p>
       </div>

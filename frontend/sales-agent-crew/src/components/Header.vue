61baf6e9033192befa049e2fc5cb397533e316e2
arund <arun.dua@sambanovasystems.com>
2025-03-10 16:15:09 +0530

    env keys added

--- frontend/sales-agent-crew/src/components/Header.vue
+++ frontend/sales-agent-crew/src/components/Header.vue
@@ -27,7 +27,7 @@
         
         <SelectProvider  v-model:selectedOption="selectedOption" />
         <!-- NEW: Chat Mode Toggle -->
-        <div  class="flex items-center space-x-2">
+        <div v-if="isWorkflowEnabled" class="flex items-center space-x-2">
           <ToggleSwitch v-model:chatMode="chatMode" label="" />
 
           <!-- <label for="modeToggle" class="text-sm text-right text-gray-600">Chat Mode</label>
@@ -94,9 +94,9 @@ import SelectProvider from '@/components/ChatMain/SelectProvider.vue'
 // import Dropdown from './Dropdown.vue'
 
 
-// const isWorkflowEnabled = computed(() => {
-//   return import.meta.env.VITE_ENABLE_WORKFLOW_TOGGLE === 'true'
-// })
+const isWorkflowEnabled = computed(() => {
+  return import.meta.env.VITE_ENABLE_WORKFLOW_TOGGLE === 'true'
+})
 
   
 
